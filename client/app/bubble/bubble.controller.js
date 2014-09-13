b5bc22e367fbeb6e244eb91f96d8d97a6372da5a
Paul L. Cimino <Paul.Cimino@capitalone.com>
2014-09-13 08:49:32 -0400

    First level animation sorted out

--- client/app/bubble/bubble.controller.js
+++ client/app/bubble/bubble.controller.js
@@ -5,15 +5,105 @@ var ggg = {};
     Position of bubbles should be dynamic, not defining bubbles and fixed div
 */
 angular.module('bubbleBaseApp')
-  .controller('BubbleCtrl', function ($scope, $rootScope, GetDataService) {
+  .controller('BubbleCtrl', function ($scope, $rootScope, GetDataService, StorageService) {
 
     var blueRange = ['CCD6F5', '99ADEB', '6685E0', '335CD6', '0033CC'];
     var redRange = ['FFCCCC', 'FFB2B2', 'FF9999', 'FF8080', 'FF6666', 'FF4D4D', 'FF3333', 'FF1919', 'FF0000'];
     var greenRange = ['01DF01', '04B404', '088A08', '0B610B', '0B3B0B', '003300'];
     var orangeRange = ['FFEBD6', 'FFE0C2', 'FFD6AD', 'FFCC99', 'FFC285', 'FFB870', 'FFAD5C', 'FFA347', 'FF9933' ];
 
+
+    $scope.lessDetail = function() {
+      $scope.setupDisplay($rootScope.currentLevel-1, $rootScope.displayProducts, $rootScope.displayServices);
+    }
+
+
+    $scope.hideAllBubbles = function() {
+      // can't simply call hide on class 'bubble' because it will show the bubbles prior to hiding
+      // so for each 'bubble' determine if it's visible, then hide
+      $('.bubble').each(function() {
+        if ($(this).attr("class").toString().indexOf("show-bubble") >= 0) {
+          $(this).addClass("hide-bubble");
+        }
+      });
+    }
+    $scope.showBubble = function(bubbleClass) {
+      console.log(bubbleClass)
+      var element = $('.' + bubbleClass);
+      element.removeClass("hide-bubble");
+      element.addClass("show-bubble");
+    }
+    $scope.hideBubble = function(bubbleClass) {
+      var element = $('.' + bubbleClass);
+      element.removeClass("show-bubble");
+      element.addClass("hide-bubble");
+    }
+
+    // TODO set this up as a prototype on array
+    var arrayHas = function(testArray, v){
+      for (i = 0; i < testArray.length; i++) {
+        if (testArray[i] === v) {
+          return true;
+        }
+      }
+      return undefined;
+    }
+
+    // Pretty much wrong, setting up bubbles and data statically
+    // in the real world bubbles would be rendered as needed
+    // and each click would run a search
+    // flattening the data so only need one ng-repeat
+    $scope.setupServiceLevel = function() {
+      // businesses persisted in address book
+      var businessList = StorageService.getAddressBook();
+      var serviceTree = [];
+      for (var k = 0; k < 15; k++) serviceTree.push({'cat':[], 'bus':[] });
+      for (var i in $rootScope.businesses) {
+        for (var j in $rootScope.businesses[i].serviceCategory) {
+          if (!arrayHas(serviceTree[j].cat, $rootScope.businesses[i].serviceCategory[j])) {
+            serviceTree[j].cat.push($rootScope.businesses[i].serviceCategory[j]);
+          }
+        }
+        var category = $rootScope.businesses[i].serviceCategory[j];
+        $rootScope.businesses[i].lastServiceCat = category;
+        j++;
+        serviceTree[j].bus.push($rootScope.businesses[i]);
+      }
+
+      $rootScope.serviceTree = serviceTree;
+    }
+    $scope.setupProductLevel = function(level) {
+     // businesses persisted in address book
+      var businessList = StorageService.getAddressBook();
+      var productTree = [];
+      for (var k = 0; k < 15; k++) productTree.push({'cat':[], 'bus':[] });
+      for (var i in $rootScope.businesses) {
+        for (var j in $rootScope.businesses[i].productCategory) {
+          if (!arrayHas(productTree[j].cat, $rootScope.businesses[i].productCategory[j])) {
+            productTree[j].cat.push($rootScope.businesses[i].productCategory[j]);
+          }
+        }
+        var category = $rootScope.businesses[i].productCategory[j];
+        $rootScope.businesses[i].lastServiceCat = category;
+        j++;
+        productTree[j].bus.push($rootScope.businesses[i]);
+      }
+
+      $rootScope.productTree = productTree;
+    }
+
+    var isInList = function(businessList, business) {
+      for (var i in businessList) {
+        if (businessList[i].id === business.id) return true;
+      }
+      return false;
+    }
+
+
     if (undefined === $rootScope.currentLevel) $rootScope.currentLevel = -1
     if (-1 === $rootScope.currentLevel) {
+      $rootScope.serviceTree = [];
+      $rootScope.productTree = [];
       $rootScope.serviceLevelCat = [];
       $rootScope.serviceLevelBus = [];
       $rootScope.productLevelCat = [];
@@ -31,98 +121,58 @@ angular.module('bubbleBaseApp')
       }
       GetDataService.get( function( database ) {
           $rootScope.businesses = database.data[0].businesses;
-          $scope.hideAllBubbles();
-          $scope.showBubble('xService');
-          $scope.showBubble('xProduct');
           $scope.setupServiceLevel();
           $scope.setupProductLevel();
       } );
-    } else {
-      $scope.setupDisplay($rootScope.currentLevel);
     }
+    $scope.setupDisplay = function(level, products, services) {
+      var displayClass = [];
 
-    $scope.setupDisplay = function(level, services, products) {
       $rootScope.currentLevel = level;
-
-
       if (level === -1) {
+          $scope.hideAllBubbles();
           $scope.showBubble('xService');
           $scope.showBubble('xProduct');
+          $scope.hideBubble('small-bubble');
       } else {
+        $scope.hideAllBubbles();
+        $scope.showBubble('small-bubble');
         $rootScope.displayProducts = products;
         $rootScope.displayServices = services;
-      }
 
+        if (products) {
+          $rootScope.productLevelBus = [];
+          $rootScope.productLevelCat = $rootScope.productTree[level].cat;
 
-    }
-
-    $scope.hideAllBubbles = function() {
-      // can't simply call hide on class 'bubble' because it will show the bubbles prior to hiding
-      // so for each 'bubble' determine if it's visible, then hide
-      $('.bubble').each(function() {
-        if ($(this).attr("class").toString().indexOf("show-bubble") >= 0) {
-          $(this).addClass("hide-bubble");
-        }
-      });
-    }
-    $scope.showBubble = function(bubbleClass) {
-      var element = $('.' + bubbleClass);
-      element.removeClass("hide-bubble");
-      element.addClass("show-bubble");
-    }
-    $scope.hideBubble = function(bubbleClass) {
-      var element = $('.' + bubbleClass);
-      element.removeClass("show-bubble");
-      element.addClass("hide-bubble");
-    }
-
-    // Pretty much wrong, setting up bubbles and data statically
-    // in the real world bubbles would be rendered as needed
-    // and each click would run a search
-    // flattening the data so only need one ng-repeat
-    $scope.setupServiceLevel = function() {
-      var levelCat = []; // data by level
-      var businessLevel = []; // businesses by level
-      for (var i in $rootScope.businesses) {
-        var lastService = $rootScope.businesses[i].serviceCategory.length - 1;
-        for (var level = 0; level < $rootScope.businesses[i].serviceCategory.length; level++) {
-          levelCat.push({'level':level+1, 'cat':$rootScope.businesses[i].serviceCategory[level]});
-          if (level === lastService) {
-            businessLevel.push({'level':level+1, 'bus':$rootScope.businesses[i]});
+          for (var i in $rootScope.productTree[level+1].bus) {
+            if (arrayHas($rootScope.productLevelCat, $rootScope.productTree[level+1].bus[i])) {
+              $rootScope.productLevelBus.push($rootScope.productTree[level+1].bus[i]);
+              displayClass.push('p'+i);
+            }
+          }
+          for (var i in $rootScope.productLevelCat) {
+            displayClass.push('productLevel'+i);
+          }
+        } else if (services) {
+          $rootScope.serviceLevelBus = [];
+          $rootScope.serviceLevelCat = $rootScope.serviceTree[level].cat;
+          for (var i in $rootScope.serviceTree[level+1].bus) {
+            if (arrayHas($rootScope.serviceLevelCat, $rootScope.serviceTree[level+1].bus[i].lastServiceCat)) {
+              $rootScope.serviceLevelBus.push($rootScope.serviceTree[level+1].bus[i]);
+              displayClass.push('s'+i);
+            }
+          }
+          for (var i in $rootScope.serviceLevelCat) {
+            displayClass.push('serviceLevel'+i);
           }
         }
-      }
 
-      $rootScope.serviceLevelCat = levelCat;
-      $rootScope.serviceLevelBus = businessLevel;
-    }
-    $scope.setupProductLevel = function(level) {
-      var levelCat = []; // data by level
-      var businessLevel = []; // businesses by level
-      for (var i in $rootScope.businesses) {
-        var lastService = $rootScope.businesses[i].productCategory.length - 1;
-        for (var level = 0; level < $rootScope.businesses[i].productCategory.length; level++) {
-          levelCat.push({'level':level+1, 'cat':$rootScope.businesses[i].productCategory[level]});
-          if (level === lastService) {
-            businessLevel.push({'level':level+1, 'bus':$rootScope.businesses[i]});
-          }
+        for (var i in displayClass) {
+          $scope.showBubble(displayClass[i]);
         }
       }
-
-      $rootScope.productLevelCat = levelCat;
-      $rootScope.productLevelBus = businessLevel;
     }
 
-    $scope.showServiceLevel = function(level) {
-      for (var i in $scope.services) {
-        var element = $('.x' + i);
-        element.style.backgroundColor = blueRange[i];
-      }
-    }
-    $scope.showProductLevel = function(level) {
-
-    }
-
-
+    $scope.setupDisplay($rootScope.currentLevel);
   });
 

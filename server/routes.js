4432f839ba0b5c624d95839704fe4901609d868f
Paul L. Cimino <p.cimino@translunardesigns.com>
2014-10-23 18:05:43 -0400

    Updated favicon and error page

--- server/routes.js
+++ server/routes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
 
   // Insert routes below
   // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+  app.route('/:url(auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
 
   // All other routes should redirect to the index.html
@@ -19,4 +19,5 @@ module.exports = function(app) {
       res.sendfile(app.get('appPath') + '/index.html');
     });
 };
+
 
